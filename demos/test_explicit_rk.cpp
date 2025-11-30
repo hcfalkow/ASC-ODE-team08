1dc5c8082a55b11b17d89365dfe0f9d9d9efd99f
Artur Andrade Do Lago <e12534826@student.tuwien.ac.at>
2025-11-30 11:13:54 +0000

    Exercise 19: ExplicitRungeKutta implementation
    - src/timestepper.hpp - added ExplicitRungeKutta class
    - demos/test_explicit_rk.cpp - new test file
    - CMakeLists.txt - added build target
    - nanoblas/src/vecexpr.hpp - bug fix (local only, line 105: isScalar<T>() -> is_scalar_type<T>::value)

---
+++ demos/test_explicit_rk.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include <nonlinfunc.hpp>
+#include <timestepper.hpp>
+#include <implicitRK.hpp>
+
+using namespace ASC_ode;
+
+
+class MassSpring : public NonlinearFunction
+{
+  double mass;
+  double stiffness;
+public:
+  MassSpring(double m, double k) : mass(m), stiffness(k) {}
+  size_t dimX() const override { return 2; }
+  size_t dimF() const override { return 2; }
+  
+  void evaluate (VectorView<double> x, VectorView<double> f) const override
+  {
+    f(0) = x(1);
+    f(1) = -stiffness/mass*x(0);
+  }
+  
+  void evaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
+  {
+    df = 0.0;
+    df(0,1) = 1;
+    df(1,0) = -stiffness/mass;
+  }
+};
+
+
+// Butcher tableau for RK2 midpoint: 2nd order, 2 stages
+Matrix<double> RK2a { { 0.0, 0.0 }, { 0.5, 0.0 } };
+Vector<> RK2b { 0.0, 1.0 };
+Vector<> RK2c { 0.0, 0.5 };
+
+// Butcher tableau for classical RK4: 4th order, 4 stages
+Matrix<double> RK4a { 
+  { 0.0, 0.0, 0.0, 0.0 },
+  { 0.5, 0.0, 0.0, 0.0 },
+  { 0.0, 0.5, 0.0, 0.0 },
+  { 0.0, 0.0, 1.0, 0.0 }
+};
+Vector<> RK4b { 1.0/6.0, 1.0/3.0, 1.0/3.0, 1.0/6.0 };
+Vector<> RK4c { 0.0, 0.5, 0.5, 1.0 };
+
+// Butcher tableau for Heun: 2nd order, 2 stages
+Matrix<double> Heuna { { 0.0, 0.0 }, { 1.0, 0.0 } };
+Vector<> Heunb { 0.5, 0.5 };
+Vector<> Heunc { 0.0, 1.0 };
+
+
+void run_test(TimeStepper& stepper, const std::string& name, double tend, int steps)
+{
+  double tau = tend/steps;
+  Vector<> y = { 1, 0 };
+  
+  std::ofstream outfile("output_" + name + ".txt");
+  outfile << 0.0 << "  " << y(0) << " " << y(1) << std::endl;
+
+  for (int i = 0; i < steps; i++)
+  {
+    stepper.DoStep(tau, y);
+    outfile << (i+1)*tau << "  " << y(0) << " " << y(1) << std::endl;
+  }
+  
+  // for mass-spring with m=k=1, exact solution is x=cos(t), v=-sin(t)
+  double exact_x = cos(tend);
+  double exact_v = -sin(tend);
+  double err_x = std::abs(y(0) - exact_x);
+  double err_v = std::abs(y(1) - exact_v);
+  
+  std::cout << name << ": steps=" << steps 
+            << " err_x=" << err_x 
+            << " err_v=" << err_v << std::endl;
+}
+
+
+int main()
+{
+  double tend = 2*M_PI;
+  auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
+  
+  std::cout << "Testing explicit Runge-Kutta methods\n" << std::endl;
+  
+  std::cout << "--- RK2 (midpoint) ---" << std::endl;
+  for (int steps : {100, 200, 400, 800})
+  {
+    ExplicitRungeKutta stepper(rhs, RK2a, RK2b, RK2c);
+    run_test(stepper, "rk2_" + std::to_string(steps), tend, steps);
+  }
+
+  std::cout << "\n--- Heun ---" << std::endl;
+  for (int steps : {100, 200, 400, 800})
+  {
+    ExplicitRungeKutta stepper(rhs, Heuna, Heunb, Heunc);
+    run_test(stepper, "heun_" + std::to_string(steps), tend, steps);
+  }
+
+  std::cout << "\n--- RK4 (classical) ---" << std::endl;
+  for (int steps : {50, 100, 200, 400})
+  {
+    ExplicitRungeKutta stepper(rhs, RK4a, RK4b, RK4c);
+    run_test(stepper, "rk4_" + std::to_string(steps), tend, steps);
+  }
+  
+  std::cout << "\n--- comparison implicit vs explicit ---" << std::endl;
+  int steps = 200;
+  {
+    ExplicitRungeKutta stepper(rhs, RK4a, RK4b, RK4c);
+    run_test(stepper, "explicit_rk4", tend, steps);
+  }
+  {
+    auto [Gauss3a,Gauss3b] = ComputeABfromC(Gauss3c);
+    ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
+    run_test(stepper, "implicit_gauss3", tend, steps);
+  }
+
+  return 0;
+}
--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -22,7 +22,7 @@ namespace ASC_ode
 
   class ExplicitEuler : public TimeStepper
   {
-    Vector<> m_vecf; // f evaluated at current step
+    Vector<> m_vecf;
   public:
     ExplicitEuler(std::shared_ptr<NonlinearFunction> rhs) 
     : TimeStepper(rhs), m_vecf(rhs->dimF()) {}
@@ -35,74 +35,104 @@ namespace ASC_ode
 
   class ImplicitEuler : public TimeStepper
   {
-    std::shared_ptr<NonlinearFunction> m_equ; // nonlinear equation to solve
-    std::shared_ptr<Parameter> m_tau; // time step size parameter
-    std::shared_ptr<ConstantFunction> m_yold; // previous time step value
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
   public:
-    ImplicitEuler(std::shared_ptr<NonlinearFunction> rhs) // constructor
-    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0)) // initialize time step size parameter
+    ImplicitEuler(std::shared_ptr<NonlinearFunction> rhs)
+    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0))
     {
-      m_yold = std::make_shared<ConstantFunction>(rhs->dimX()); // previous time step value
-      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX()); // current time step value
-      m_equ = ynew - m_yold - m_tau * m_rhs; // implicit Euler equation
-    }
-
-    void DoStep(double tau, VectorView<double> y) override // perform one time step
-    {
-      m_yold->set(y); // set previous time step value
-      m_tau->set(tau); // set time step size
-      NewtonSolver(m_equ, y); // solve nonlinear equation for new value
-    }
-  };
-
-  // Improved Euler:
-  // y_tilde = y_n + tau/2 * f(y_n)
-  // y_{n+1} = y_n + tau * f(y_tilde)
-  class ImprovedEuler : public TimeStepper // improved Euler method
-  {
-    Vector<> m_vecf; // f evaluated at current step
-    Vector<> m_ytilde; // intermediate value
-  public:
-    ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
-    : TimeStepper(rhs), m_vecf(rhs->dimF()), m_ytilde(rhs->dimX()) {} // constructor
-    void DoStep(double tau, VectorView<double> y) override // perform one time step
-    {
-      this->m_rhs->evaluate(y, m_vecf); // evaluate f(y_n)
-      m_ytilde = y + (tau/2.0) * m_vecf; // compute y_tilde
-      this->m_rhs->evaluate(m_ytilde, m_vecf); // evaluate f(y_tilde)
-      y += tau * m_vecf; // update y to y_{n+1}
-    }
-  };
-
-  // Crank-Nicolson:
-  // y_{i+1} = y_i + tau/2 * (f(t_i, y_i) + f(t_{i+1}, y_{i+1}))
-  class CrankNicolson : public TimeStepper
-  {
-    std::shared_ptr<NonlinearFunction> m_equ; // nonlinear equation to solve
-    std::shared_ptr<Parameter> m_tau; // time step size parameter
-    std::shared_ptr<ConstantFunction> m_yold; // previous time step value
-    std::shared_ptr<ConstantFunction> m_fold; // f evaluated at previous step
-  public:
-    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) // constructor
-    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0))  // initialize time step size parameter
-    {
-      m_yold = std::make_shared<ConstantFunction>(rhs->dimX()); // previous time step value initialized as constant function
-      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX()); // current time step value initialized as identity
-      m_fold = std::make_shared<ConstantFunction>(rhs->dimF()); // f evaluated at previous step initialized as constant function
-      m_equ = ynew - m_yold - m_tau * (m_rhs + m_fold); // crank-nicolson equation
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      m_equ = ynew - m_yold - m_tau * m_rhs;
     }
 
     void DoStep(double tau, VectorView<double> y) override
     {
-      m_yold->set(y); // set yold to previous y value
-      this->m_rhs->evaluate(y, m_fold->get()); // evaluate f at previous y value
-      m_tau->set(tau/2.0); // set time step size parameter to tau/2
-      NewtonSolver(m_equ, y); // set y to solution of nonlinear equation
+      m_yold->set(y);
+      m_tau->set(tau);
+      NewtonSolver(m_equ, y);
     }
   };
 
+  class ImprovedEuler : public TimeStepper
+  {
+    Vector<> m_vecf;
+    Vector<> m_ytilde;
+  public:
+    ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_vecf(rhs->dimF()), m_ytilde(rhs->dimX()) {}
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      this->m_rhs->evaluate(y, m_vecf);
+      m_ytilde = y + (tau/2.0) * m_vecf;
+      this->m_rhs->evaluate(m_ytilde, m_vecf);
+      y += tau * m_vecf;
+    }
+  };
+
+  class CrankNicolson : public TimeStepper
+  {
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
+    std::shared_ptr<ConstantFunction> m_fold;
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs)
+    : TimeStepper(rhs), m_tau(std::make_shared<Parameter>(0.0))
+    {
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      m_fold = std::make_shared<ConstantFunction>(rhs->dimF());
+      m_equ = ynew - m_yold - m_tau * (m_rhs + m_fold);
+    }
+
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      m_yold->set(y);
+      this->m_rhs->evaluate(y, m_fold->get());
+      m_tau->set(tau/2.0);
+      NewtonSolver(m_equ, y);
+    }
+  };
+
+  // Explicit RK for arbitrary Butcher tableau (a must be strictly lower triangular)
+  class ExplicitRungeKutta : public TimeStepper
+  {
+    Matrix<> m_a;         // Butcher tableau coefficients
+    Vector<> m_b, m_c;
+    int m_stages;
+    int m_n;              // dimension of ODE system
+    Vector<> m_k;         // stores all k^j values concatenated
+    Vector<> m_ytmp;
+  public:
+    ExplicitRungeKutta(std::shared_ptr<NonlinearFunction> rhs,
+      const Matrix<> &a, const Vector<> &b, const Vector<> &c) 
+    : TimeStepper(rhs), m_a(a), m_b(b), m_c(c),
+      m_stages(c.size()), m_n(rhs->dimX()), 
+      m_k(m_stages * m_n), m_ytmp(m_n)
+    { }
+
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      m_k = 0.0;
+      
+      // compute k^j = f(y + tau * sum_{l<j} a_{jl} * k^l)
+      for (int j = 0; j < m_stages; j++)
+      {
+        m_ytmp = y;
+        for (int l = 0; l < j; l++)  // lower triangular: sum only over l < j
+          m_ytmp += tau * m_a(j,l) * m_k.range(l*m_n, (l+1)*m_n);
+        
+        m_rhs->evaluate(m_ytmp, m_k.range(j*m_n, (j+1)*m_n));
+      }
+      
+      // y_{n+1} = y_n + tau * sum_j b_j * k^j
+      for (int j = 0; j < m_stages; j++)
+        y += tau * m_b(j) * m_k.range(j*m_n, (j+1)*m_n);
+    }
+  };
 
 }
-
 
 #endif
